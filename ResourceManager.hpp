4254e94931de99b400014abdd77c6c4f027bab7a
Igor Lemieszka <01171665@pw.edu.pl>
2024-11-03 18:31:29 +0100

    12

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "Resource.hpp"
 #include <iostream>
 #include <memory>
@@ -13,35 +12,10 @@ public:
     {
         std::cout << "ResourceManager created" << std::endl;
     }
-
     ~ResourceManager() { std::cout << "ResourceManager destroyed" << std::endl; }
-
-    double get() const { return resource->get(); }
-
-    ResourceManager(const ResourceManager& other)
-    {
-        if (other.resource) {
-            resource = std::make_unique< Resource >(*other.resource);
-        }
-        std::cout << "ResourceManager copied" << std::endl;
-    }
-
-    ResourceManager& operator=(const ResourceManager& other)
-    {
-        if (this != &other) {
-            if (other.resource) {
-                resource = std::make_unique< Resource >(*other.resource);
-            }
-            else {
-                resource.reset();
-            }
-        }
-        std::cout << "ResourceManager assigned" << std::endl;
-        return *this;
-    }
-
-    ResourceManager(const ResourceManager&)            = delete; 
-    ResourceManager& operator=(const ResourceManager&) = delete; 
-    ResourceManager(ResourceManager&&) noexcept        = default;     
-    ResourceManager& operator=(ResourceManager&&) noexcept = default; 
+    double get() { return resource->get(); }
+    ResourceManager(const ResourceManager&)                = delete;
+    ResourceManager& operator=(const ResourceManager&)     = delete;
+    ResourceManager(ResourceManager&&) noexcept            = default;
+    ResourceManager& operator=(ResourceManager&&) noexcept = default;
 };
