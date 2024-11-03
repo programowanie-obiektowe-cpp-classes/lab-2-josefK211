dbdf1677fe50f9128691427932a955f1a8b78631
Igor Lemieszka <01171665@pw.edu.pl>
2024-11-03 18:31:29 +0100

    1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "Resource.hpp"
 #include <iostream>
 #include <memory>
@@ -6,7 +5,7 @@
 class ResourceManager
 {
 private:
-    std::unique_ptr< Resource > resource;
+    std::unique_ptr< Resource > resource; 
 
 public:
     ResourceManager() : resource(std::make_unique< Resource >())
@@ -16,32 +15,34 @@ public:
 
     ~ResourceManager() { std::cout << "ResourceManager destroyed" << std::endl; }
 
-    double get() const { return resource->get(); }
+    double get() { return resource->get(); }
 
     ResourceManager(const ResourceManager& other)
     {
         if (other.resource) {
-            resource = std::make_unique< Resource >(*other.resource);
+            resource =
+                std::make_unique< Resource >(*other.resource); 
         }
         std::cout << "ResourceManager copied" << std::endl;
     }
 
     ResourceManager& operator=(const ResourceManager& other)
     {
-        if (this != &other) {
-            if (other.resource) {
-                resource = std::make_unique< Resource >(*other.resource);
-            }
-            else {
-                resource.reset();
-            }
+        if (this == &other) {
+            return *this; 
+        }
+
+        if (other.resource) {
+            resource =
+                std::make_unique< Resource >(*other.resource); 
+        }
+        else {
+            resource.reset(); 
         }
         std::cout << "ResourceManager assigned" << std::endl;
         return *this;
     }
 
-    ResourceManager(const ResourceManager&)            = delete; 
-    ResourceManager& operator=(const ResourceManager&) = delete; 
-    ResourceManager(ResourceManager&&) noexcept        = default;     
-    ResourceManager& operator=(ResourceManager&&) noexcept = default; 
+    ResourceManager(ResourceManager&&) noexcept = default;
+    ResourceManager& operator=(ResourceManager&&) noexcept = default;
 };
