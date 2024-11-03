15f236127c4b5bbfc9012071d740ebf743d9c505
Igor Lemieszka <01171665@pw.edu.pl>
2024-11-03 21:17:25 +0100

    1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,56 +1,24 @@
 #include "Resource.hpp"
 #include <iostream>
+#include <memory>
 
 class ResourceManager
 {
 private:
-    Resource* resource; 
+    std::unique_ptr< Resource > resource;
 
 public:
-    ResourceManager() : resource(new Resource())
+    ResourceManager() : resource(std::make_unique< Resource >())
     {
         std::cout << "ResourceManager created" << std::endl;
     }
+
     ~ResourceManager() { std::cout << "ResourceManager destroyed" << std::endl; }
-<<<<<<<<< Temporary merge branch 1
 
     double get() { return resource->get(); }
 
-    double get() const { return resource->get(); }
-
-    ResourceManager(const ResourceManager& other)
-    {
-        if (other.resource) {
-            resource =
-                std::make_unique< Resource >(*other.resource); 
-        }
-        std::cout << "ResourceManager copied" << std::endl;
-    }
-
-    ResourceManager& operator=(const ResourceManager& other)
-    {
-        if (this == &other) {
-            return *this; 
-        }
-
-        if (other.resource) {
-            resource =
-                std::make_unique< Resource >(*other.resource); 
-        }
-        else {
-            resource.reset(); 
-        }
-        std::cout << "ResourceManager assigned" << std::endl;
-        return *this;
-    }
-};
-
-    ResourceManager(ResourceManager&&) noexcept = default;
-=========
-    double get() { return resource->get(); }
     ResourceManager(const ResourceManager&)                = delete;
     ResourceManager& operator=(const ResourceManager&)     = delete;
     ResourceManager(ResourceManager&&) noexcept            = default;
->>>>>>>>> Temporary merge branch 2
     ResourceManager& operator=(ResourceManager&&) noexcept = default;
 };
